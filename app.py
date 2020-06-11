554f862183d9ead4d011dbe548894685117823c8
Q. Price <priceqp@gmail.com>
2020-06-11 09:06:15 -0500

    complete step two, add jQuery and ajax

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ from boggle import Boggle
 
 app = Flask(__name__)
 boggle_game = Boggle()
+# Configure Key for Session
+app.config["SECRET_KEY"] = "oh-so-secret"
 
 
 @app.route("/")
 def show_game():
     """Display Board & Form | Start Session"""
 
-    board = boggle_game.make_board()
+    if "board" in session:
+
+        board = session.get("board")
+
+    else:
+
+        board = boggle_game.make_board()
+
+        session["board"] = board
 
     return render_template("board.html", board=board)
---
+++ static/app.js
@@ -0,0 +1,5 @@
+// Handle Guess Submission
+$("#guessForm").on("submit", function (event) {
+  event.preventDefault();
+  console.log("Submitted");
+});
