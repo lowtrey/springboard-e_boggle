d76ddfebdd3d54c0926bd8c739241d959c165b27
Q. Price <priceqp@gmail.com>
2020-06-11 20:32:49 -0500

    complete step five

--- static/app.js
+++ static/app.js
@@ -1,3 +1,26 @@
+$(window).on("load", function () {
+  const timer = setInterval(countdown, 1000);
+
+  function countdown() {
+    // TODOS:
+    // - Change timer text to red when under 20 seconds
+    // - Slide Guess form up when time is up
+    // - slide restart button down when time is up
+    $("#seconds").text(`${parseInt($("#seconds").text()) - 1}`);
+
+    if ($("#seconds").text() === "20") {
+      $("#seconds").css("color", "khaki");
+    } else if ($("#seconds").text() === "10") {
+      $("#seconds").css("color", "lightcoral");
+    } else if ($("#seconds").text() === "0") {
+      clearInterval(timer);
+      $("#seconds").text("Time's Up!");
+      $("#guessForm").slideUp();
+      $("#restart").delay(1000).slideDown();
+    }
+  }
+});
+
 // Validate Guessed Word
 const validateGuess = async (guessedWord) => {
   const response = await axios.get("/guess", {
@@ -21,6 +44,12 @@ const generateMessage = (guessedWord, responseText) => {
   return message;
 };
 
+const showMessage = (message) => {
+  // TODO: chain the lines below together
+  $("#message").text(message);
+  $("#message").slideDown().delay(1000).slideUp();
+};
+
 // Handle Guess Form Submission
 $("#guessForm").on("submit", async function (event) {
   event.preventDefault();
@@ -31,5 +60,31 @@ $("#guessForm").on("submit", async function (event) {
 
   const message = generateMessage($guess, guessResponse);
 
-  $("#message").text(message);
+  const wordFound = guessResponse === "ok";
+
+  showMessage(message);
+
+  if (wordFound) updateScore();
+});
+
+// Score Functionality
+const $scoreBox = $("#scoreBox");
+const $score = $("#score");
+
+function updateScore() {
+  // initialize score to zero
+  if (!$score.text()) {
+    $score.text("0");
+  } else {
+    // update score - animate if first point
+    $score.text(`${parseInt($score.text()) + 1}`);
+    if ($score.text() === "1") {
+      $scoreBox.slideToggle();
+    }
+  }
+}
+
+// Reset Game (not board)
+$("#restartButton").on("click", function () {
+  location.reload();
 });
